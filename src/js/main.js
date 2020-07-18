6a8c3d312403c92fd0ad888d372142038eaff26c
ivancraze <ivancraze@yandex.ru>
2020-07-18 23:02:32 +0400

    added tabs, forms, modals, calc

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 import "./slider";
+import modals from './modules/modals';
+import tabs from './modules/tabs';
+import forms from './modules/forms';
+import changeModalState from './modules/changeModalState';
 
-console.log(1);
+window.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+
+    let modalState = {};
+
+    changeModalState(modalState);
+    modals();
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
+    forms(modalState);
+});
